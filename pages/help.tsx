83a61a302080504053645bf852ecf4a19129edb8
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-12-27 18:56:40 +0400

    Create pages and fill index page with created components

--- pages/help.tsx
+++ pages/help.tsx
@@ -2,22 +2,19 @@ import Head from "next/head";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import UseCase from "@/components/UseCase";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>CrassulaPay - Home</title>
+        <title>CrassulaPay - Help</title>
         <meta name="description" content="CrassulaPay" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.png" />
       </Head>
-      <Header isHome />
+      <Header />
       <main className="main">
-        <Hero isHome />
-        <UseCase />
+        <div className="container">Help Page</div>
       </main>
       <Footer />
     </>
--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,23 +1,21 @@
 import Head from "next/head";
+import Image from "next/image";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import Hero from "@/components/Hero";
-import UseCase from "@/components/UseCase";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>CrassulaPay - Home</title>
+        <title>CrassulaPay - Pricing</title>
         <meta name="description" content="CrassulaPay" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/logo.png" />
       </Head>
-      <Header isHome />
+      <Header />
       <main className="main">
-        <Hero isHome />
-        <UseCase />
+        <div className="container">Pricing Page</div>
       </main>
       <Footer />
     </>
