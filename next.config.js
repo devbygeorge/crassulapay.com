7822616a6480e5dfa31bec0d8e35f3cdb797f73f
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-12-27 16:27:55 +0400

    Add env example file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+  i18n: {
+    locales: ["en", "ge"],
+    defaultLocale: "en",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
