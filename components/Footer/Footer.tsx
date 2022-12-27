44888c7e5b622bff091e50973f8712070cbb7914
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2022-12-27 16:36:43 +0400

    Add header & footer components

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,5 @@
+import s from "./Footer.module.scss";
+
+export default function Footer() {
+  return <footer className={s.footer}>footer</footer>;
+}
---
+++ components/Header/Header.tsx
@@ -0,0 +1,5 @@
+import s from "./Header.module.scss";
+
+export default function Header() {
+  return <header className={s.header}>header</header>;
+}
