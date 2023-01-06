a788d287307dbbef0fc88c5f424f10b56fd01aa4
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-01-06 20:15:32 +0400

    Update UseCases content

--- components/UseCases/UseCases.tsx
+++ components/UseCases/UseCases.tsx
@@ -1,4 +1,6 @@
 import s from "./UseCases.module.scss";
+import { BiSupport } from "react-icons/bi";
+import { AiFillBank } from "react-icons/ai";
 
 export default function UseCases() {
   return (
@@ -14,93 +16,116 @@ export default function UseCases() {
           </p>
         </div>
       </div>
+      {/* Block 1 */}
       <div className={s.block}>
         <div className={s.block_text}>
           {/* Block Heading */}
-          <h2>Keep your money with Crassula.</h2>
-          {/* Block Item 1 */}
-          <h3>Receive your salary, pension, and more.</h3>
+          <h2>Pay and bag with Crassula worldwide.</h2>
+          {/* Block Paragraph */}
           <p>
-            Relocate without the stress — and without the multiple bank
-            accounts. Share your details with your employer, pension scheme,
-            family or friends, and get paid like a local.
-          </p>
-          {/* Block Item 2 */}
-          <h3>Spend in local currency with your card.</h3>
-          <p>
-            Avoid the bank appointments, and start spending as soon as you get
-            there. You’ll always get the best possible exchange rate.
-          </p>
-          {/* Block Item 3 */}
-          <h3>Move your money between countries.</h3>
-          <p>
-            Send money to 80 countries, always with a low and transparent fee.
-            With Direct Debits in the UK, Europe, US and Canada, paying your
-            bills and subscriptions across currencies is easier than ever.
+            Crassula is the most convenient method to shop online. We gave a
+            secure, reliable payment system that will keep your mind at peace.
+            You can pay for most goods and services online except the prohibited
+            items listed in the initial agreement.
           </p>
         </div>
         <div className={s.block_image}>
-          <img src="/images/usecase-1.jpg" alt="usecase 1" />
+          <img src="/images/usecase-1.jpeg" alt="usecase 1" />
         </div>
       </div>
+      {/* Block 2 */}
       <div className={s.block}>
         <div className={s.block_image}>
           <img src="/images/usecase-2.jpg" alt="usecase 2" />
         </div>
         <div className={s.block_text}>
           {/* Block Heading */}
-          <h2>Pay with crassula worldwide.</h2>
-          {/* Block Item 1 */}
-          <h3>Pay at the real exchange rate.</h3>
+          <h2>How to get Crassula card?</h2>
+          {/* Block Paragraph */}
           <p>
-            Avoid sneaky bank exchange rate markups and high foreign transaction
-            fees. Pay only a low upfront conversion fee, and no annual card fee.
-          </p>
-          {/* Block Item 2 */}
-          <h3>Always pay in the local currency.</h3>
-          <p>
-            If you’re shopping online, always choose to pay in the currency of
-            the country you’re buying from. If you don’t have it in your
-            account, the card will automatically convert to it from the account
-            that’s cheapest for you. You’ll avoid Dynamic Currency Conversion
-            fees.
-          </p>
-          {/* Block Item 3 */}
-          <h3>Pay online securely with instant notifications.</h3>
-          <p>
-            Authorise payments with 3-D Secure authentication. Receive instant
-            notifications when you spend.
+            Register securely with Crassula in order to receive a Crassula card
+            by post at your home address. You need to go through the
+            identification process. We require your selfie as well as proof of
+            address, and a copy of your passport to be uploaded to our system
+            for the identity verification process. You need to fill in the
+            personal information questionnaire. Generated code will be sent to
+            your phone number for additional verification. After successful
+            verification, you will receive a verification confirmation letter as
+            well as a welcome letter with your account details.
           </p>
         </div>
       </div>
+      {/* Block 3 */}
       <div className={s.block}>
         <div className={s.block_text}>
           {/* Block Heading */}
-          <h2>Crassula makes life comfortable.</h2>
-          {/* Block Item 1 */}
-          <h3>Save when you buy property abroad.</h3>
+          <h2>Keep your money with Crassula.</h2>
+          {/* Block Paragraph */}
           <p>
-            Sending and managing money with CrassulaPay is on average 6x cheaper
-            than UK banks. And with our tiered pricing for large amounts, you
-            get an even lower fee on any transfers over 100,000 GBP or
-            equivalent in your currency.
-          </p>
-          {/* Block Item 2 */}
-          <h3>Pay your bills remotely.</h3>
-          <p>
-            Hold 52 currencies in one place. Easily manage your mortgage and
-            utility bills across currencies with Direct Debit in the UK, Europe,
-            US and Canada, and pay tradespeople like a local.
-          </p>
-          {/* Block Item 3 */}
-          <h3>Receive payments from tenants.</h3>
-          <p>
-            Get paid in 9 currencies with your own account details. And when the
-            rate is right, convert your money in seconds.
+            Transfer your money from your bank account to your Crassula account.
+            Dollar and euro transfers are accepted. Keep your money with
+            Crassula account free of charges (transfer fees are applied initial
+            agreement) accordingly to.
           </p>
         </div>
         <div className={s.block_image}>
-          <img src="/images/usecase-4.jpeg" alt="usecase 3" />
+          <img src="/images/usecase-3.jpeg" alt="usecase 3" />
+        </div>
+      </div>
+      {/* Block 4 */}
+      <div className={s.block}>
+        <div className={s.block_image}>
+          <img src="/images/usecase-4.jpg" alt="usecase 4" />
+        </div>
+        <div className={s.block_text}>
+          {/* Block Heading */}
+          <h2>Paying methods.</h2>
+          {/* Block Paragraph */}
+          <p>
+            You can pay securely with the touch and ping method as well as with
+            the contactless payment method. The monthly limit for touch and ping
+            and contactless payments will be applied accordingly to the initial
+            agreement and Crassula's terms and conditions.
+          </p>
+        </div>
+      </div>
+      {/* Block 5 */}
+      <div className={s.block}>
+        <div className={s.block_text}>
+          {/* Block Heading */}
+          <h2>Transfer your money to another account and withdrawal.</h2>
+          {/* Block Paragraph */}
+          <p>
+            Transfer the money from your Crassula to any other account worldwide
+            a secure and convenient way (transfer fees applied accordingly to
+            the initial agreement) monthly limit for the transfer will be
+            applied. You can withdraw money from your Crassula card at any ATM
+            worldwide supported by Visa or MasterCard. Daily and monthly limits
+            of withdrawal will be applied accordingly to the initial agreement.
+          </p>
+        </div>
+        <div className={s.block_image}>
+          <img src="/images/usecase-5.jpg" alt="usecase 4" />
+        </div>
+      </div>
+      <div className={s.double}>
+        <div className="container">
+          <div className={s.item}>
+            <AiFillBank />
+            <h2>Online Banking</h2>
+            <p>
+              We will provide personal online banking login details for your
+              comfort and convenience.
+            </p>
+          </div>
+          <div className={s.item}>
+            <BiSupport />
+            <h2>Supporting & Help.</h2>
+            <p>
+              Our site is provided with 24-hour live chat support as well as
+              24-hour hotline support.
+            </p>
+          </div>
         </div>
       </div>
     </section>
