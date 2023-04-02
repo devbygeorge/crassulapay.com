f9a8ec6f3951c2cd01660f98b37c4bca0d5dc0b1
GiorgiPasieshvili <giopasieshvili2@gmail.com>
2023-04-02 16:07:02 +0400

    Update gitignore & next config for sanity

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   typescript: {
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["cdn.sanity.io"],
   },
   i18n: {
     locales: ["en", "ru", "ka"],
